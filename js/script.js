880594169153b380b4cb18918b60b5c1bbc3c367
Midhun M S <midhunreal1@gmail.com>
2023-08-19 01:39:09 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,77 +1,106 @@
+
+const form = document.getElementById("signup-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const password1Input = document.getElementById("password1");
+const password2Input = document.getElementById("password2");
+const errorMessage = document.getElementById("message");
+const successMessage = document.getElementById("success");
+const phoneNumberPattern = /^[0-9]{10}$/;
+
+
 function formValid() {
-
-    // declarations
-    const message = document.getElementById('message');
-    const successMessage = document.getElementById('success');
-    const fullName = document.getElementById('name').value;
-    const mail = document.getElementById('email').value;
-    const phone = document.getElementById('phone');
-    const pass1 = document.getElementById('password1').value;
-    const pass2 = document.getElementById('password2').value;
-    const head = document.getElementById('head');
-    const nameSection = document.getElementsByClassName('name-section');
-    message.style.display = "none";
-    successMessage.style.display = "none";
-    var exp= /^[0-9]{10}$/;
-    document.body.scrollTop=0;
-    document.documentElement.scrollTop=0;
-// conditions
-
-    if (fullName.length == 0 || fullName.length==1) {
-      message.innerText = "Enter your Full name";
-      message.style.display = "block";
-      return false;
+    let valid = true;
+    if (!nameInput.value.trim() || !emailInput.value.trim() || !phoneInput.value.trim() || !password1Input.value.trim() || !password2Input.value.trim()) {
+        valid = false;
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Please fill in all fields.";
+    } else if (password1Input.value !== password2Input.value) {
+        valid = false;
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Passwords do not match.";
+    } else {
+        errorMessage.style.display = "none";
     }
-    else if (
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail) == false
-    ) {
-      message.innerText = "Enter correct email id";
-      message.style.display = "block";
-      return false;
-    }
-    else if(!phone.value.match(exp)){
-        message.innerText = "Enter valid phone number";
-        message.style.display = "block";
-        return false;
-    }
-    else if (pass1.length < 8) {
-      message.innerText = "Password length must be greater than 8";
-      message.style.display = "block";
-      return false;
-    }
-    else if(pass2!=pass1){
-        message.innerText = "Passwords don't match";
-      message.style.display = "block";
-        return false;
-    }
-else {
-      successMessage.innerText = "You have successfully created an account";
-      successMessage.style.display = "block";
-      alert("Success!! You can now login");
-      window.open("login.html");
-      return false;
-    }
+    return valid
+    
+}
+phoneInput.addEventListener("input", function() {
+  if (phoneNumberPattern.test(phoneInput.value)) {
+      phoneInput.setCustomValidity("");
+  } else {
+      phoneInput.setCustomValidity("Please enter a valid 10-digit phone number");
   }
+});
 
-  function loginFormValid(){
-    const message = document.getElementById('message');
-    const mail = document.getElementById('email').value;
-    const pass1 = document.getElementById('password').value;
-    if (
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail) == false
-      ) {
-        message.innerText = "Enter correct email id";
-        message.style.display = "block";
-        return false;
-      }
-    else if (pass1.length < 8) {
-        message.innerText = "Password incorrect";
-        message.style.display = "block";
-        return false;
-      }
-      else{
-        window.alert('You are now logged in')
-        window.open('index.html');
-        return false;
-      }
+
+// Password strength meter
+
+  password1Input.addEventListener("input", () => {
+    const password = password1Input.value;
+    const strengthMeter = document.getElementById("password-strength-meter");
+    const strengthText = document.getElementById("password-strength-text");
+    const strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    const mediumRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{6,})/;
+    
+    if (strongRegex.test(password)) {
+        strengthMeter.value = 4;
+        strengthText.innerText = "Strong";
+        strengthText.style.color = "green";
+
+    } else if (mediumRegex.test(password)) {
+        strengthMeter.value = 2;
+        strengthText.innerText = "Medium";
+        strengthText.style.color = "orange";
+        
+    } else {
+        strengthMeter.value = 1;
+        strengthText.innerText = "Weak";
+        strengthText.style.color = "red";
+       
+    }
+});
+
+function getStrength(password) {
+  const strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  const mediumRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{6,})/;
+
+  if (strongRegex.test(password)) {
+      return "strong";
+  } else if (mediumRegex.test(password)) {
+      return "medium";
+  } else {
+      return "weak";
   }
+}
+
+
+// Clear password 
+form.addEventListener("reset", () => {
+    const strengthMeter = document.getElementById("password-strength-meter");
+    const strengthText = document.getElementById("password-strength-text");
+    strengthMeter.value = 0;
+    strengthText.innerText = "";
+    strengthText.style.color = "";
+});
+document.addEventListener("DOMContentLoaded", function() {
+  form.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const password = password1Input.value; // Get the password value
+      const passwordStrength = getStrength(password); // Pass the password to the function
+      console.log(passwordStrength);
+
+      if (passwordStrength === "weak") {
+          errorMessage.style.display = "block";
+          errorMessage.innerText = "Please choose a stronger password.";
+          return 0;
+      }
+
+      if (formValid()) {
+          window.location.href = "success.html";
+          return;
+      }
+  });
+});
